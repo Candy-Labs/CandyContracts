940a54967ddbbd7d84c6260f4ad00a65e3bafc5c
Alex Sikand <alex@silklabs.ai>
2022-02-28 09:19:38 -0800

    Fix incorrect check for new tokenURI method

--- contracts/token/ERC721/ERC721A.sol
+++ contracts/token/ERC721/ERC721A.sol
@@ -6,7 +6,6 @@ pragma solidity ^0.8.4;
 import '@openzeppelin/contracts/token/ERC721/IERC721.sol';
 import '@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/IERC721Metadata.sol';
-import '@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol';
 import '@openzeppelin/contracts/utils/Address.sol';
 import '@openzeppelin/contracts/utils/Strings.sol';
 import '@openzeppelin/contracts/utils/introspection/ERC165.sol';
